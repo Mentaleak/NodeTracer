4b8035c6c3e3448572ce2d1da3be0bfa448a3096
Max <Benchmaker@users.noreply.github.com>
2020-02-29 18:00:37 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 const path = require('path');
 const colors = require('colors');
 
-function log(msg) {
-    if(traceon){
+function log(msg, traceon) {
+    if (traceon) {
         var date = new Date();
         var hour = date.getHours();
         hour = (hour < 10 ? "0" : "") + hour;
-        var min  = date.getMinutes();
+        var min = date.getMinutes();
         min = (min < 10 ? "0" : "") + min;
-        var sec  = date.getSeconds();
+        var sec = date.getSeconds();
         sec = (sec < 10 ? "0" : "") + sec;
         var year = date.getFullYear();
         var month = date.getMonth() + 1;
         month = (month < 10 ? "0" : "") + month;
-        var day  = date.getDate();
+        var day = date.getDate();
         day = (day < 10 ? "0" : "") + day;
         var stamp = year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
 
@@ -23,8 +23,11 @@ function log(msg) {
         Error.captureStackTrace(err, arguments.callee);
         const callee = err.stack[0];
         Error.prepareStackTrace = orig;
-        console.log(` ${stamp} `.black.bgWhite, `${path.relative(process.cwd(), callee.getFileName())}:${callee.getLineNumber()}: `.bold.blue+ `${msg}`);
+        console.log(` ${stamp} `.black.bgWhite, `${path.relative(process.cwd(), callee.getFileName())}:${callee.getLineNumber()}: `.bold.blue + `${msg}`);
+    } else {
+        console.log("Trace mode is disabled");
     }
 }
-var traceon=true;
-log("test")
+module.exports = {
+    log
+};
